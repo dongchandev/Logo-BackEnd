1243e7cf89b11cadcda73831d9fda49f62ecc2e9
김동찬 <129833370+dongchandev@users.noreply.github.com>
2023-12-26 16:54:46 +0900

    :sparkles: :: querydsl 기본 설정 완료

---
+++ Logo-infra/persistence/persistence-commons/src/main/java/com/hj/persistencecommons/support/QueryDSLHelperSupport.java
@@ -0,0 +1,9 @@
+package com.hj.persistencecommons.support;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+public class ExpressionSupport {
+    public static class Suggest{
+        public static BooleanExpression 
+    }
+}
